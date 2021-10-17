d254c91f7d10fa1fc9733903eb59cde71e24f76e
Natars84 <pasero.romain@gmail.com>
2021-10-17 12:02:21 +0200

    Suppression de la partie buggée et recodage
    Suite à de nombreux bugs trouvés dans la partie téléchargement. Suppression complète de cette portion de script.
    
    La portion de script recodée va envoyer une première requête afin de récupérer les résultats sous forme de fichier JSON.
    De ce fichier, le script va extraire le nombre d'occurrences trouvées et demander à l'utilisateur s'il souhaite télécharger les images.
    Si oui, le téléchargement se lance (représenté par le print() à la fin du script).
    
    Mise en place d'une fonction d'envoi de requête avec contrôle de sa bonne exécution:
     - Si la requête échoue, un message d'erreur s'affiche indiquant la requête en cause, ainsi que le code de retour HTTP si possible.
    
    Mise à jour des commentaires afin de rajouter les accents pris en charge par python 3.

--- imagesNasa.py
+++ imagesNasa.py
@@ -1,17 +1,19 @@
 import requests
+from requests.adapters import HTTPadapter
+from requests.package.urllib3.util.retry import Retry
 import json
 import sys
 import os
 
-urlAPI_NASA = "https://images-api.nasa.gov/search?"
-urlRequete = urlAPI_NASA
+urlRequete = "https://images-api.nasa.gov/search?"
 
-dossierImage = "/home/pi/imageNASA/"
+#Le dossier créé sera là où se trouve le script
+dossierImage = os.getcwd() + "/imageNASA/"
 
-#On tente de creer le dossier d'accueil des documents
+#On tente de créer le dossier d'accueil des documents
 try:
 	os.mkdir(dossierImage)
-except OSError: #Si erreur: il existe deja
+except OSError: #Si erreur: il existe déjà
 	pass
 
 ##########################
@@ -21,136 +23,113 @@ critereRecherche = dict()
 argumentRecu = dict()
 listeCritereRecherche = ['q', 'center', 'description', 'keywords', 'location', 'media_type', 'nasa_id', 'photographer', 'secondary_creator', 'title', 'year_start', 'year_end']
 
-#On recupere les arguments
+#On récupère les arguments
 listeArgument = sys.argv
 listeArgument.pop(0) #Suppression du nom du script dans la liste des arguments
 
-#On procede au decoupage des arguments en fonction du '='
+#On procède au découpage des arguments en fonction du '='
 for argument in listeArgument:
 	argumentDecoupe = argument.split('=')
 
-	#Pour chacun des arguments, on verifie si il y a une correspondance avec l'un
-	#des criteres de recherche possible
+	#Pour chacun des arguments, on vérifie si il y a une correspondance avec l'un
+	#des critères de recherche possible
 	for critereRecherchePossible in listeCritereRecherche:
 		if critereRecherchePossible == argumentDecoupe[0]:
-			#Si tel est le cas, on place le critere et sa valeur dans le
-			#dictionnaire des criteres recus
+			#Si tel est le cas, on place le critère et sa valeur dans le
+			#dictionnaire des critères recus
 			critereRecherche[argumentDecoupe[0]] = argumentDecoupe[1]
 
 ############################
 #Construction de la requete#
 ############################
 
-#Une fois tous les arguments receptionnes, on fait les changements necessaires afin
-#qu'ils puissent etre envoyes dans une requete HTTP
+#Une fois tous les arguments réceptionnés, on fait les changements necessaires afin
+#qu'ils puissent être envoyés dans une requête HTTP
 for cle, valeur in critereRecherche.items():
 	critereRecherche[cle] = valeur.replace(" ", "%20")
 
-#Une fois termine, on construit la requete HTTP
+#Une fois terminé, on construit la requête HTTP
 for cle, valeur in critereRecherche.items():
 	urlRequete = urlRequete + cle + "=" + valeur + "&"
 
-#On supprime le dernier caractere (&) inutilise
+#On supprime le dernier caractère (&) inutilise
 urlRequete = urlRequete[0:-1]
 
 
-############################
-#Recuperation et traitement#
-#   du fichier JSON recu   #
-############################
+###########################
+#  Requête et traitement  #
+#   des données reçues    #
+###########################
 
-#La fonction extrait dans le fichier JSON le nombre TOTAL de documents trouves
-def nombreResultatTrouve(resultatRequeteGET):
-        json = resultatRequeteGET.json()
+# Permet d'exécuter une requête
+def envoyerRequeteGET(url):
+	try:
+		session = requests.Session()
+		retry = Retry(connect=3, backoff_factor=0.5)
+		adapter = HTTPadapter(max_retries = retry)
+		session.mount('http://', adapter)
+		session.mount('https://', adapter)
 
-	#On extrait le nombre de resultats trouves par le site web
-        return json["collection"]["metadata"]["total_hits"]
+		reponse = session.get()
 
-#La fonction extrait du fichier JSON les liens vers le "sous fichier" JSON contenant
-#un ou plusieurs liens (en fonction du type de doucument) vers le document
-def extraireLienVersCollectionJSON(fichierJSON):
-	lienFichierCollectionJSON = []
+	except:
+		print("La dernière requête envoyée a rencontré une erreur")
+		print("Requête: " + url)
+		exit()
 
-	#On recupere les resultats sous forme de fichier JSON
-	items = fichierJSON["collection"]["items"]
+	statutHTTP = statutRequeteCorrect(reponse)
+	if statutHTTP:
+		return reponse
 
-	#On determine le nombre de resultats presents dans le fichier JSON
-	nombreResultatATraiter = len(items)
-
-	#Pour chacun des resultat, on extrait l'URL du fichier "collection.json"
-	for numeroResultat in range(nombreResultatATraiter):
-		lienFichierCollectionJSON.append(items[numeroResultat]["href"])
-
-	#Une fois le fichier traite, on renvoie la liste generee
-	return lienFichierCollectionJSON
-
-#On decoupe la chaine en fonction du . et recupere l'extension
-def extensionFichierURL(urlFichierDistant):
-	chaineDecoupe = urlFichierDistant.split('.')
-	extensionFichier = chaineDecoupe[len(chaineDecoupe) - 1]
-
-	return str(extensionFichier)
-
-def telechargerDocument(lienATraiter, nomFichier):
-
-	#On verifie que chaque lien est accessible puis on le telecharge
-	reponse = requests.get(lienATraiter)
-	if reponse.status_code == 200:
-		nomFichier = dossierImage + str(nomFichier) + "." + extensionFichierURL(lienATraiter)
-		with open(nomFichier, "wb") as file:
-			file.write(reponse.content)
-			return 1
 	else:
-		return 0
+		print("La dernière requête envoyée a rencontré une erreur")
+		print("Requête: " + url)
+		print("Statut HTTP: " + statutHTTP)
+		exit()
 
-#On envoie la requete et recupere les resultats
-reponseServeur = requests.get(urlRequete)
+# Vérifie que le code de retour HTTP soit correct
+def statutRequeteCorrect(reponseRequete):
+	if reponseRequete.status_code >= 200 and reponseRequete.status_code <= 399:
+		return True
+	else:
+		return False
 
-if reponseServeur.status_code >= 400 and reponseServeur.status_code < 500:
-	sys.exit('Erreur de requete')
-elif reponseServeur.status_code >= 500:
-	sys.exit('Erreur au niveau du serveur, veuillez reessayer ulterieurement')
-else:
-	pass
+# On récupère les données trouvées par le serveur suivant les critères de recherche fournis
+reponse = envoyerRequeteGET(urlRequete)
 
-#On verifie si on a trouve quelque chose
-nombreResultat = nombreResultatTrouve(reponseServeur)
-if nombreResultat == 0:
-	sys.exit('Aucun resultat trouve')
-else:
-	print nombreResultat, ' documents trouves'
+# On décode les données JSON reçues sous forme de dicrionnaire
+donnees = reponse.json()
 
-print 'Debut du telechargement'
+# Récupération du nombre d'éléments trouvés
+nombreOccurence = int(donnees["collection"]["metadata"]["total_hits"])
 
-#Si on a des resultats, alors on recupere les liens vers les "sous-fichiers" JSON
-#contenant le lien direct vers le document sous un ou plusieurs formats.
-#Le premier lien est le document original (entre autres pour les images)
-nombreDocumentATelecharger = nombreResultat
-numeroPage = 1
+# On affiche le nmbre de résultats trouvés et on demande à l'utilisateur s'il faut lancer le téléchargement
+# La boucle est là pour s'assurer que l'on obtiens une réponse correcte
+while True:
+	valeurRecueCorrecte = True
 
-urlRequete = urlRequete + '&page='
+	print(str(nombreOccurence) + "éléments trouvés. Voulez-vous lancer le téléchargement ?")
+	print("1: OUR ; 2: NON")
+	choix = input("Réponse: ")
 
-while nombreDocumentATelecharger > 0:
-	#On fait une nouvelle requete en indiquant la page de resultat
-	#sur laquelle travailler
-	reponseServeur = requests.get(urlRequete + str(numeroPage))
+	# Une fois que l'utilisateur a fait son choix, on détermine s'il est correcte
+	# CàD qu'il s'agisse d'un nombre compris entre 1 et 2
+	try:
+		choix = int(choix)
+	except:
+		valeurRecueCorrecte = False
 
-	for lienFichierCollectionJSON in extraireLienVersCollectionJSON(reponseServeur.json()):
-		#On recupere le fichier collection.json
-		reponseServeur = requests.get(lienFichierCollectionJSON)
-
-		#On extrait le 1er lien du fichier (document original)
-		json = reponseServeur.json()
-		lienATelecharger = json[0]
-		nomFichierATelecharger = (nombreResultat - nombreDocumentATelecharger) + 1
-
-		#On telecharge le fichier et on indique le resultat du telechargement
-		telechargementReussi = telechargerDocument(lienATelecharger, nomFichierATelecharger)
-		if telechargementReussi:
-			print 'Document ', nomFichierATelecharger, ': OK'
+		if choix == 1:
+			print("\nOK, lancement du téléchargement")
+			break
+		elif choix == 2:
+			print("\nOK, abandon du téléchargement et fin du programme")
+			exit()
 		else:
-			print 'Document ', nomFichierATelecharger, ': echec'
+    			valeurRecueCorecte = False
 
-		nombreDocumentATelecharger -= 1
-
-	numeroPage += 1
+		# Si la valeur saisie par l'utilisateur est incorrecte
+		if not valeurRecueCorrecte:
+			print("\nLa valeur saisie est incorrecte, veuillez réessayer")
+			
+print("Downloooooooooooooooooooooooooooooooooooooooooooooooooooad !!!")
